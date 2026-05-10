cba0775fb85bfd9b91ae87ed8144edb165238424
alex-rosales0704 <72619083+alex-rosales0704@users.noreply.github.com>
2026-05-10 02:10:37 -0600

    Update MateriasController.cs

--- Controllers/MateriasController.cs
+++ Controllers/MateriasController.cs
@@ -19,13 +19,13 @@ namespace ColegioSanJose.Controllers
             _context = context;
         }
 
-        // GET: Materias
+       
         public async Task<IActionResult> Index()
         {
             return View(await _context.Materias.ToListAsync());
         }
 
-        // GET: Materias/Details/5
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -43,15 +43,13 @@ namespace ColegioSanJose.Controllers
             return View(materia);
         }
 
-        // GET: Materias/Create
+    
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Materias/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("MateriaId,NombreMateria,Docente")] Materia materia)
@@ -65,7 +63,7 @@ namespace ColegioSanJose.Controllers
             return View(materia);
         }
 
-        // GET: Materias/Edit/5
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +79,7 @@ namespace ColegioSanJose.Controllers
             return View(materia);
         }
 
-        // POST: Materias/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("MateriaId,NombreMateria,Docente")] Materia materia)
@@ -116,7 +112,7 @@ namespace ColegioSanJose.Controllers
             return View(materia);
         }
 
-        // GET: Materias/Delete/5
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +130,6 @@ namespace ColegioSanJose.Controllers
             return View(materia);
         }
 
-        // POST: Materias/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
